120b138ba776b81ef3fb9d00ae457b7cce77d2b7
Marcos <marcospalomero21@gmail.com>
2026-02-10 20:00:01 +0100

    update

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,4 +1,4 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +6,20 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // new cdk.aws_s3.CfnBucket(this, 'MyBucket', {
+    //   bucketName: 'bucket-marpalpir21',
     // });
+
+    const bucket = new cdk.aws_s3.Bucket(this, 'MyBucket', {
+      bucketName: 'bucket-marpalpir21',
+      versioned: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Elimina el bucket al destruir la pila
+      autoDeleteObjects: true, // Elimina los objetos dentro del bucket al destruir la pila
+    });
+
+    new cdk.aws_s3_deployment.BucketDeployment(this, 'DeployBucket', {
+      sources: [cdk.aws_s3_deployment.Source.asset('./assets')],
+      destinationBucket: bucket,
+    });
   }
 }
